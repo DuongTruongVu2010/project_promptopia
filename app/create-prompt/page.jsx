08c83d8a08c83b884324531bb1a956b1f19844a5
DuongTruongVu2010 <duongtruongvu20102004@gmail.com>
2024-08-15 17:37:38 +0700

    latest change

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { useState } from "react";
@@ -8,46 +9,30 @@ import Form from "@components/Form";
 
 const CreatePrompt = () => {
   const router = useRouter();
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
+
   const [submitting, setIsSubmitting] = useState(false);
   const [post, setPost] = useState({ prompt: "", tag: "" });
-
-  // Check if session is still loading
-  if (status === "loading") {
-    return <div>Loading...</div>;
-  }
 
   const createPrompt = async (e) => {
     e.preventDefault();
     setIsSubmitting(true);
 
-    if (!session?.user?.id) {
-      console.error("User ID is missing in session.");
-      setIsSubmitting(false);
-      return;
-    }
-
     try {
       const response = await fetch("/api/prompt/new", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
         body: JSON.stringify({
           prompt: post.prompt,
-          userId: session.user.id,
+          userId: session?.user.id,
           tag: post.tag,
         }),
       });
 
       if (response.ok) {
         router.push("/");
-      } else {
-        const errorData = await response.json();
-        console.error("Failed to create prompt:", errorData);
       }
     } catch (error) {
-      console.error("Error occurred while creating prompt:", error);
+      console.log(error);
     } finally {
       setIsSubmitting(false);
     }
