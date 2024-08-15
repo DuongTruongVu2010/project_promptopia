7fc634dc9efed0bddc899a794cb7f6547b741ba9
DuongTruongVu2010 <duongtruongvu20102004@gmail.com>
2024-08-15 16:57:48 +0700

    first commit

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,64 @@
+import Prompt from "@models/prompt";
+import mongoose from "mongoose";
+import { connectToDB } from "@utils/database";
+
+export const GET = async (request, { params }) => {
+  try {
+    await connectToDB();
+
+    const prompt = await Prompt.findById(params.id).populate("creator");
+    if (!prompt) return new Response("Prompt Not Found", { status: 404 });
+
+    return new Response(JSON.stringify(prompt), { status: 200 });
+  } catch (error) {
+    return new Response("Internal Server Error !!!", { status: 500 });
+  }
+};
+
+export const PATCH = async (request, { params }) => {
+  const { prompt, tag } = await request.json();
+
+  try {
+    await connectToDB();
+
+    // Find the existing prompt by ID
+    const existingPrompt = await Prompt.findById(params.id);
+
+    if (!existingPrompt) {
+      return new Response("Prompt not found", { status: 404 });
+    }
+
+    // Update the prompt with new data
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+
+    await existingPrompt.save();
+
+    return new Response("Successfully updated the Prompts", { status: 200 });
+  } catch (error) {
+    return new Response("Error Updating Prompt", { status: 500 });
+  }
+};
+
+export const DELETE = async (request, { params }) => {
+  try {
+    await connectToDB();
+
+    // Kiểm tra tính hợp lệ của ObjectId
+    if (!mongoose.Types.ObjectId.isValid(params.id)) {
+      return new Response("Invalid prompt ID", { status: 400 });
+    }
+
+    // Tìm và xóa prompt theo ID
+    const deletedPrompt = await Prompt.findByIdAndDelete(params.id);
+
+    if (!deletedPrompt) {
+      return new Response("Prompt not found", { status: 404 });
+    }
+
+    return new Response("Prompt deleted successfully", { status: 200 });
+  } catch (error) {
+    console.error("Error deleting prompt:", error);
+    return new Response("Error deleting prompt", { status: 500 });
+  }
+};
---
+++ app/profile/page.jsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const [myPosts, setMyPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setMyPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, [session?.user.id]);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {
+    // Xác nhận từ người dùng trước khi xóa
+    const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
+  
+    if (hasConfirmed) {
+      try {
+        // Thực hiện yêu cầu DELETE
+       await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+        
+        // Lọc và cập nhật danh sách bài viết
+        const filteredPosts = myPosts.filter((item) => item._id !== post._id);
+        setMyPosts(filteredPosts);
+      } catch (error) {
+        console.log("Error deleting post:", error);
+      }
+    }
+  };
+  
+
+  return (
+    <Profile
+      name='My'
+      desc='Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination'
+      data={myPosts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default MyProfile;
